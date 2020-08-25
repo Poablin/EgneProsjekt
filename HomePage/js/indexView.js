0f5de5dd86938d69d3d49634ccd74497504297ab
Poablin <kristoffer.elvejord@gmail.com>
2020-08-25 19:28:51 +0200

    Header workin

--- HomePage/js/indexView.js
+++ HomePage/js/indexView.js
@@ -4,11 +4,14 @@ const show = () => {
     contentDiv.innerHTML = `
     <div class="gridContent" id="gridContent">
         <div class="header">
-            <ul style="margin-right:50px;">
-                <li class="headerList"><a href="">Andre ting</a></li>
-                <li class="headerList"><a href="#">Andre ting</a></li>
+            <div class="logo">Hey</div>
+            <div class="listDiv">
+                <ul>
                 <li class="headerList"><a href="pages/Ludo/game.html">Ludo</a></li>
-            </ul>
+                <li class="headerList"><a href="#">Andre ting</a></li>
+                <li class="headerList"><a href="#">Andre ting</a></li>
+                </ul>
+            </div>
         </div>
         <div class="main" id="main"></div>
         <div class="footer"></div>
