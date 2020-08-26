4fe953ef91d66f4ec7152d3aa09701764daafe94
Poablin <kristoffer.elvejord@gmail.com>
2020-08-26 15:08:34 +0200

    Fiksa class

--- HomePage/js/indexView.js
+++ HomePage/js/indexView.js
@@ -6,10 +6,10 @@ const show = () => {
         <div class="header">
             <div class="logoDiv"><span>Eventuell logo her</span></div>
             <div class="listDiv">
-                <ul>
-                <li class="headerList"><a href="pages/Ludo/game.html">Ludo</a></li>
-                <li class="headerList"><a href="#">Andre ting</a></li>
-                <li class="headerList"><a href="#">Andre ting</a></li>
+                <ul class="headerList">
+                <li><a href="pages/Ludo/game.html">Ludo</a></li>
+                <li><a href="#">Funker ikke enda</a></li>
+                <li><a href="#">Funker ikke enda</a></li>
                 </ul>
             </div>
         </div>
