4cebeda0da8441f74c907379a4cf24f0fee248a0
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-20 14:54:44 +0200

    uppy

---
+++ CSharpSnake/CSharpSnake/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpSnake
+{
+    class Board
+    {
+        internal Board()
+        {
+            Console.WriteLine("┌───────────────────────┐");
+            Console.WriteLine("│                       │");
+            Console.WriteLine("│            S          │");
+            Console.WriteLine("│                       │");
+            Console.WriteLine("│                       │");
+            Console.WriteLine("│                       │");
+            Console.WriteLine("└───────────────────────┘");
+        }
+    }
+}
---
+++ CSharpSnake/CSharpSnake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = new Board();
+        }
+    }
+}
