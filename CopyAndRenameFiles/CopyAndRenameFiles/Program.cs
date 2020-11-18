9de42f73417cfd5c5f284fe3b69a9bea77a6534b
Poablin <65161335+Poablin@users.noreply.github.com>
2020-11-18 23:31:39 +0100

    Update

---
+++ CopyAndRenameFiles/CopyAndRenameFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace CopyAndRenameFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathfile = @"C:\Users\krist\Downloads\CIA output";
+
+            string[] filePaths = Directory.GetFiles(pathfile);
+
+            foreach (string filePath in filePaths)
+            {
+                try
+                {
+                    string dire = Path.GetDirectoryName(filePath);
+                    string name = Path.GetFileNameWithoutExtension(filePath);
+                    string exte = Path.GetExtension(filePath);
+                    name = name.Substring(6);
+                    Console.WriteLine("Copying file for: " + name);
+                    File.Copy($"{filePath}", $"{pathfile}\\Output\\{name}-New{exte}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error File Copy");
+                }
+            }
+        }
+    }
+}
